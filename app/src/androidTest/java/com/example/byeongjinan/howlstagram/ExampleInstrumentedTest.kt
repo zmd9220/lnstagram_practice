cdccc968e1e9393fa300dcf0d38646b693b96af4
zmd9220 <31811018+zmd9220@users.noreply.github.com>
2020-04-28 18:23:43 +0900

    make with androidX
    make google login

--- app/src/androidTest/java/com/example/byeongjinan/howlstagram/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/byeongjinan/howlstagram/ExampleInstrumentedTest.kt
@@ -1,7 +1,11 @@
 package com.example.byeongjinan.howlstagram
 
-import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+//import android.support.test.InstrumentationRegistry // deprecated
+//import android.support.test.runner.AndroidJUnit4 // deprecated
+//import androidx.test.ext.junit.runners.AndroidJUnit4 // 추가 : deprecated 대체
+//import androidx.test.platform.app.InstrumentationRegistry //추가 : deprecated 대체
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -18,7 +22,8 @@ class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
+//        val appContext = InstrumentationRegistry.getTargetContext() // androidX에서 사용이 안됨 제거
+        var appContext = InstrumentationRegistry.getInstrumentation().targetContext // 추가 : deprecated 대체
         assertEquals("com.example.byeongjinan.howlstagram", appContext.packageName)
     }
 }

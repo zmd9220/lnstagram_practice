077c43ff2c2bf9685c4316f6baa9950f0f13c0cb
zmd9220 <31811018+zmd9220@users.noreply.github.com>
2020-05-05 19:52:06 +0900

    make with androidX make add photo upload page complete

--- app/src/main/java/com/example/byeongjinan/howlstagram/MainActivity.kt
+++ app/src/main/java/com/example/byeongjinan/howlstagram/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.example.byeongjinan.howlstagram
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import com.example.byeongjinan.howlstagram.navigation.AlarmFragment
-import com.example.byeongjinan.howlstagram.navigation.DetailViewFragment
-import com.example.byeongjinan.howlstagram.navigation.GridFragment
-import com.example.byeongjinan.howlstagram.navigation.UserFragment
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.byeongjinan.howlstagram.navigation.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
@@ -16,6 +18,9 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bottom_navigation.setOnNavigationItemSelectedListener(this)
+
+        // 6장 사진 업로드 할때 필요한 권한인 파일을 읽어올 수 있는 권한 얻기
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),1)
     }
 
     override fun onNavigationItemSelected(p0: MenuItem): Boolean {
@@ -31,6 +36,10 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
                 return true
             }
             R.id.action_add_photo->{
+                // 먼저 외부 저장소를 가져오기(읽기) 권한을 가지고 있는지 확인하고 권한이 있으면 AddPhotoActivity로 넘어가기
+                if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                    startActivity(Intent(this,AddPhotoActivity::class.java))
+                }
                 return true
             }
             R.id.action_favorite_alarm->{
--- app/src/main/java/com/example/byeongjinan/howlstagram/navigation/AddPhotoActivity.kt
+++ app/src/main/java/com/example/byeongjinan/howlstagram/navigation/AddPhotoActivity.kt
@@ -5,9 +5,12 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.byeongjinan.howlstagram.R
 import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_add_photo.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AddPhotoActivity : AppCompatActivity() {
 // 위치 스캔 코드생성 (시스템 내부 확인) + storage라는 변수(저장소), 이미지사진 url를 담을 수 있는 변수 생성
@@ -46,4 +49,18 @@ class AddPhotoActivity : AppCompatActivity() {
             }
         }
     }
+    fun contentUpload(){
+        //Make filename 파일이름을 만들어 주는 코드
+        var timestamp =  SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        var imageFileName = "IMAGE" + timestamp + "_.png" // 중복생성 x(시간으로)
+
+        var storageRef = storage?.reference?.child("images")?.child(imageFileName) //폴더명
+
+        //FileUpload
+        // 성공시 토스트(화면에 성공탭 잠깐 떴다 사라지는 거) !!-null satefy제거?
+        storageRef?.putFile(photoUri!!)?.addOnSuccessListener {
+            Toast.makeText(this,getString(R.string.upload_success),Toast.LENGTH_LONG).show()
+        }
+    }
+
 }

032f536392b09427fb9134431190cac3b6ae3386
zmd9220 <31811018+zmd9220@users.noreply.github.com>
2020-05-06 12:07:59 +0900

    make with androidX, kotlin make content data model(userID, uid, timestamp, explain, imageURL ...) database model

--- app/src/main/java/com/example/byeongjinan/howlstagram/navigation/AddPhotoActivity.kt
+++ app/src/main/java/com/example/byeongjinan/howlstagram/navigation/AddPhotoActivity.kt
@@ -7,6 +7,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.byeongjinan.howlstagram.R
+import com.example.byeongjinan.howlstagram.navigation.model.ContentDTO
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_add_photo.*
 import java.text.SimpleDateFormat
@@ -17,12 +20,18 @@ class AddPhotoActivity : AppCompatActivity() {
     var PICK_IMAGE_FROM_ALBUM = 0
     var storage : FirebaseStorage? = null
     var photoUri : Uri? = null
+    // 유저 정보를 가져올 수 있도록 하는 변수 7장추가
+    var auth : FirebaseAuth? = null
+    // 데이터 매스를 사용할 수 있도록 firestore 변수에 정보 가져오기 7장추가
+    var firestore : FirebaseFirestore? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_photo)
 
-        //Initiate storage
+        //Initiate storage, auth, firestore 정보 가져옴 7장추가(2줄)
         storage = FirebaseStorage.getInstance()
+        auth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
 
         //Open the album 이 액티비티를 실행하자마자 바로 화면을 오픈해주는 코드
         var photoPickerIntent = Intent(Intent.ACTION_PICK)
@@ -56,10 +65,34 @@ class AddPhotoActivity : AppCompatActivity() {
 
         var storageRef = storage?.reference?.child("images")?.child(imageFileName) //폴더명
 
+
         //FileUpload
-        // 성공시 토스트(화면에 성공탭 잠깐 떴다 사라지는 거) !!-null satefy제거?
+        // 성공시 토스트(화면에 성공탭 잠깐 떴다 사라지는 거) !!-null satefy제거? (6장)
+//        storageRef?.putFile(photoUri!!)?.addOnSuccessListener {
+//            Toast.makeText(this,getString(R.string.upload_success),Toast.LENGTH_LONG).show()
+//        }
+        // 데이터 베이스를 입력해줄 코드를 넣어주기 Callback method (7장)
         storageRef?.putFile(photoUri!!)?.addOnSuccessListener {
-            Toast.makeText(this,getString(R.string.upload_success),Toast.LENGTH_LONG).show()
+            // 이미지 업로드 완료 됐으면 이미지 다운로드 주소를 받아오는 코드
+            storageRef.downloadUrl.addOnSuccessListener { uri ->
+                // 주소를 받아오자 마자 데이터 모델을 만들기
+                var contentDTO = ContentDTO()
+
+                // Insert downloadURI of image
+                contentDTO.imageURL = uri.toString()
+
+                // Insert uid of user
+                contentDTO.uid = auth?.currentUser?.uid
+
+                // Insert userID
+                contentDTO.userID = auth?.currentUser?.email
+
+                // Insert explain of content 내용 설명 사용자 입력글과 설명글이 들어감
+                contentDTO.explain = addphoto_edit_explain.text.toString()
+
+                // Insert timestamp 밀리세컨드
+                contentDTO.timestamp = System.currentTimeMillis()
+            }
         }
     }
 
---
+++ app/src/main/java/com/example/byeongjinan/howlstagram/navigation/model/ContentDTO.kt
@@ -0,0 +1,10 @@
+package com.example.byeongjinan.howlstagram.navigation.model
+
+// 사진을 체계적으로 데이터로써 관리할 컨텐츠 데이터 모델 만들기
+// 컨텐츠의 설명을 관리하는 explain 변수 이미지 주소를 관리하는 imageURL 변수 어느 아이디가 올렸는지 관리할 uid 변수 올린 유저의 이미지를 관리할 userID
+// 몇시 몇분에 올렸는지 확인할 timestamp 좋아요를 몇개 눌렸는지 알려주는 favoriteCount 중복 좋아요를 방지할 수 있도록 좋아요를 누른 유저를 관리할 수 있는 favorites(map 자료구조)
+data class ContentDTO(var explain : String? =null, var imageURL : String? = null, var uid : String? = null, var userID : String? = null,
+var timestamp : Long? =null, var favoriteCount : Int = 0, var favorites : Map<String,Boolean> = HashMap()) {
+    // 이 게시글에 달린 덧글을 관리하는 클래스 uid=id userID=이메일? comment 댓글 timestamp 올린 시간
+    data class Comment(var uid : String? = null, var userID : String? = null, var comment : String? = null, var timestamp : Long? = null)
+}
